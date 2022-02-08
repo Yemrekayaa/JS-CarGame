752ac10d07433837c89b68f5a71a676989177d2e
Yunus Emre Kaya <yemrekayatr@gmail.com>
2022-02-08 17:36:52 +0300

    BIG UPDATE

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            pause();
+        } else {
+            //resume();
+        }
+    }, false);
+
     const car = document.querySelector('.car');
     const game_area = document.querySelector('.game-area');
     const road = document.querySelector('.road');
+    const pauseElement = document.querySelector('.pause');
+    const scoreElement = document.querySelector('.score');
+    const startElement = document.querySelector('.start');
 
     let myCar = {
-        width: 78,
-        height: 166,
-        bottom: 25,
+        width: 64,
+        height: 148,
+        top: 500,
         left: 225,
         speed: 7,
+        score: 0,
+        isPause: false,
+        isGameOver: false,
         leftTime: null,
         rightTime: null
     }
+    
 
 
+    function score() {
+        if(myCar.isPause == false){
+        myCar.score += 1;
+        scoreElement.innerHTML = parseInt(myCar.score);
+    }
+    }
+
+        let scoreTime = setInterval(score, 100);
     function carControl(direction) {
-        if ((myCar.left >= 20 || direction != "left")&& (myCar.left <= 400|| direction == "left")) {
+        if ((myCar.left >= 25 || direction != "left") && (myCar.left <= 410 || direction == "left")) {
             let move = direction == "left" ? -1 : 1;
             myCar.left += move;
             car.style.left = myCar.left + "px";
@@ -24,18 +47,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function keyControl(e) {
-        if (e.keyCode === 65 && !e.repeat && e.type == "keydown") {
-            clearInterval(myCar.rightTime);
-            clearInterval(myCar.leftTime);
-            myCar.leftTime = setInterval(() => carControl("left"), myCar.speed);
-        }
-        else if (e.keyCode === 68 && !e.repeat && e.type == "keydown") {
-            clearInterval(myCar.rightTime);
-            clearInterval(myCar.leftTime);
-            myCar.rightTime = setInterval(() => carControl("right"), myCar.speed);
-        } else if ((e.keyCode === 68 || e.keyCode === 65) && e.type == "keyup") {
-            clearInterval(myCar.rightTime);
-            clearInterval(myCar.leftTime);
+        if (myCar.isPause == false) {
+            if (e.keyCode === 65 && !e.repeat && e.type == "keydown") {
+                clearInterval(myCar.rightTime);
+                clearInterval(myCar.leftTime);
+                myCar.leftTime = setInterval(() => carControl("left"), myCar.speed);
+            }
+            else if (e.keyCode === 68 && !e.repeat && e.type == "keydown") {
+                clearInterval(myCar.rightTime);
+                clearInterval(myCar.leftTime);
+                myCar.rightTime = setInterval(() => carControl("right"), myCar.speed);
+            } else if ((e.keyCode === 68 || e.keyCode === 65) && e.type == "keyup") {
+                clearInterval(myCar.rightTime);
+                clearInterval(myCar.leftTime);
+            }
+            if (e.keyCode === 32 && !e.repeat && e.type == "keydown") {
+                myCar.isPause = true;
+                pause();
+            }
+        } else if (e.keyCode === 32 && !e.repeat && e.type == "keydown" && myCar.isGameOver == false) {
+            myCar.isPause = false;
+            resume();
+        } else if(e.keyCode === 32 && !e.repeat && e.type == "keydown" && myCar.isGameOver == true){
+            location.reload();
         }
     }
     document.addEventListener("keydown", keyControl)
@@ -43,54 +77,89 @@ document.addEventListener('DOMContentLoaded', () => {
     
 
 
-    function generateCar(){
+    function generateCar() {
         let enemyCar = {
-            width: 78,
-            height: 166,
-            left : 30,
+            width: 64,
+            height: 148,
+            left: 30,
             top: -166,
-            pos: [35,150,272,390],
             speed: 1
         }
-        
-        let randomLeft= enemyCar.pos[Math.floor(Math.random() * 4)];
+        let randomPos = [35, 150, 272, 390];
+        enemyCar.left = randomPos[Math.floor(Math.random() * 4)];
         const enemyCarElement = document.createElement("div");
         enemyCarElement.classList.add('enemy-car')
         road.appendChild(enemyCarElement);
-        enemyCarElement.style.left = randomLeft + "px";
+        enemyCarElement.style.left = enemyCar.left + "px";
 
-        function moveEnemyCar(){
-            
-            enemyCar.top += enemyCar.speed;
-            enemyCarElement.style.top = enemyCar.top  + "px";
-            
-            if(enemyCar.top >= 564){
-               
+        function moveEnemyCar() {
+            if (myCar.isPause == false) {
+                enemyCar.top += enemyCar.speed;
+                enemyCarElement.style.top = enemyCar.top + "px";
+
+            }
+
+
+            if (enemyCar.top >= 583 && myCar.isPause == false) {
+
                 enemyCar.height -= enemyCar.speed;
                 enemyCarElement.style.height = enemyCar.height + "px";
-                if(enemyCar.height <= 0){
+                if (enemyCar.height <= 0) {
                     road.removeChild(enemyCarElement);
                     clearInterval(moveTimer)
                 }
                 
             }
 
-            if(enemyCar.bottom == 191){
-                gameOver();
+            if ((enemyCar.top >= (myCar.top - myCar.height) && enemyCar.top <= (myCar.top + myCar.height)) &&
+                ((myCar.left >= enemyCar.left && myCar.left <= enemyCar.left + enemyCar.width) ||
+                    (myCar.left + myCar.width >= enemyCar.left && myCar.left + myCar.width <= enemyCar.left + enemyCar.width))) {
+                if (myCar.isGameOver == false) gameOver();
             }
         }
-        
-        
+
+        let moveTimer = setInterval(moveEnemyCar, 10);
 
 
-        let moveTimer = setInterval(moveEnemyCar,10);
-
-        
+    }
+    let generateTimer = setInterval(generateCar, 3500);
+    function gameOver() {
+        myCar.isPause = true;
+        myCar.isGameOver = true;
+        road.style.animationPlayState = "paused";
+        clearInterval(generateTimer);
+        clearInterval(myCar.rightTime);
+        clearInterval(myCar.leftTime);
+        pauseElement.style.display = "block";
+        pauseElement.innerHTML = "GAME OVER" + "<p class='press'>(press spacebar to play again)</p>";
     }
 
-    function gameOver(){
-        console.log("bitti");
+    function pause() {
+        myCar.isPause = true;
+        road.style.animationPlayState = "paused";
+        clearInterval(generateTimer);
+        clearInterval(myCar.rightTime);
+        clearInterval(myCar.leftTime);
+        pauseElement.style.display = "block";
+        pauseElement.innerHTML = "PAUSED" + "<p class='press'>(press spacebar to continue)</p>";
     }
 
-    setInterval(generateCar,4000);
+    function resume() {
+        myCar.isPause = false;
+        road.style.animationPlayState = "running";
+        generateTimer = setInterval(generateCar, 3500);
+        pauseElement.style.display = "none";
+    }
+    start();
+    function start(){
+        myCar.isPause = true;
+        road.style.animationPlayState = "paused";
+        clearInterval(generateTimer);
+        clearInterval(myCar.rightTime);
+        clearInterval(myCar.leftTime);
+        pauseElement.style.display = "block";
+        pauseElement.innerHTML = "START GAME" + "<p class='press'>(press spacebar to start)</p>";
+    }
+
+    
 })
